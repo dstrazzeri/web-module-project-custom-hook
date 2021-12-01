487cc61020675b3b82b827eb910a2f260002c0b8
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-12-01 15:22:27 -0500

    that was easier than I thought it would be

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import useLocalStorage from "./useLocalStorage";
+
+
+const useDarkMode = () => {
+    const [darkMode, setDarkMode]  = useLocalStorage( false);
+    return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
